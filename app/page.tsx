41c6aa33735e503ad4c8f90126fdb01d074b109c
Lee Ray Hao <oahyar@gmail.com>
2024-04-15 15:54:27 +0800

    Change of wallpaper
    change background image

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const frameMetadata = getFrameMetadata({
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    src: `${NEXT_PUBLIC_URL}/park-3.jpg`,
     aspectRatio: '1:1',
   },
   input: {
